ad8cff55858f78b2a72931b1a4b8365206be0017
Holly Holland <hhollyolland@gmail.com>
2017-12-06 20:41:31 -0700

    invoices...1!!

---
+++ INTEX/Controllers/Invoices1Controller.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INTEX.Models
+{
+    public class Invoices1Controller : Controller
+    {
+        private northwestContext db = new northwestContext();
+
+        // GET: Invoices1
+        public ActionResult Index()
+        {
+            var invoices = db.Invoices.Include(i => i.Order);
+            return View(invoices.ToList());
+        }
+
+        // GET: Invoices1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices1/Create
+        public ActionResult Create()
+        {
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "OrderComments");
+            return View();
+        }
+
+        // POST: Invoices1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InvoiceID,OrderID,PaymentDueDate,EarlyPaymentDate,EarlyPaymentDiscount,SentDate,AdvancedAmount,RunningBalance")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Invoices.Add(invoice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "OrderComments", invoice.OrderID);
+            return View(invoice);
+        }
+
+        // GET: Invoices1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "OrderComments", invoice.OrderID);
+            return View(invoice);
+        }
+
+        // POST: Invoices1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InvoiceID,OrderID,PaymentDueDate,EarlyPaymentDate,EarlyPaymentDiscount,SentDate,AdvancedAmount,RunningBalance")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(invoice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "OrderComments", invoice.OrderID);
+            return View(invoice);
+        }
+
+        // GET: Invoices1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
+
+        // POST: Invoices1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Invoice invoice = db.Invoices.Find(id);
+            db.Invoices.Remove(invoice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
